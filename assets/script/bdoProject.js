21384fd05dde1161ca186ce50c614d82bdd96008
Marcos  Gregório <86073733+marcos-gregs@users.noreply.github.com>
2022-12-04 17:30:59 -0300

    attContanteLanguage

---
+++ assets/script/bdoProject.js
@@ -0,0 +1,38 @@
+// Lista de Projetos
+const project = [
+        {
+        name:'Sunnyside',
+        tag:'front-end sass',
+        image:'sunny.png',
+        link:'https://vercel.com/trona-vidro/sunnyside/'
+        },
+        {
+        name:'Ide photo',
+        tag:'front-end',
+        image:'ide.png',
+        link:'https://ide-photos-zeta.vercel.app/'
+        },
+        {
+        name:'PortofóliWeb',
+        tag:'front-end sass',
+        image:'potWev.png',
+        link:'https://portfolio-website-main-nu.vercel.app/'
+        },{
+        name:'Curriculo Web',
+        tag:'front-end bulma',
+        image:'cvWeb.png',
+        link:'https://vercel.com/trona-vidro/sunnyside/'
+        },
+        {
+        name:'oneBitX',
+        tag:'front-end Bootstrap',
+        image:'oneBit.png',
+        link:'https://one-bit-x-j1idytsou-trona-vidro.vercel.app/'
+        },
+        {
+        name:'news-homepage-main',
+        tag:'front-end',
+        image:'blog.png',
+        link:'https://news-homepage-main-omega.vercel.app/'
+        },
+]
